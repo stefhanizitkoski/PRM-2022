4ddcdbe1ff9e8f98df94d247fe502f67a01d9caf
Stéfhani Zitkoski <stefhani.zitkoski@gmail.com>
2022-08-09 21:45:51 -0300

    Aula 09/08/2022

---
+++ loja-backend/src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { Brand } from "./entity/Brand"
+import { Category } from "./entity/Category"
+import { Product } from "./entity/Product"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "prmdb",
+    synchronize: true,
+    logging: false,
+    entities: [Brand, Category, Product],
+})
--- loja-backend/src/server.ts
+++ loja-backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { AppDataSource } from './data-source';
+import cors from 'cors';
 
 //Instancio uma aplicação express
 const app = express();
@@ -6,7 +8,18 @@ const app = express();
 //determina a porta de execução
 const PORT = 3300;
 
-app.listen(PORT, () => {
-    console.log(`Running in port ${PORT}`);
-    console.log('teste');
-})
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Se conectar no banco de dados, levanto a aplicação
+AppDataSource.initialize().then(() => {
+
+    //Levanto a aplicação
+    app.listen(PORT, () => {
+        console.log(`Server running in port ${PORT}`);
+    })
+
+}).catch(error => {
+    console.log('Ops, não conectei no banco de dados', error);
+});
